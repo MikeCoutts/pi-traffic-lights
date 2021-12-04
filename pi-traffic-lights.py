b5c039724a94f09ad3270f8c6bab202a91b69447
MikeCoutts <Mikec@kavinjka.com>
2021-12-04 07:39:36 +0000

    Conform to PEP 8 for function names

--- pi-traffic-lights.py
+++ pi-traffic-lights.py
@@ -1,6 +1,6 @@
 # Copyright The Kavinjka Software Company 2021
 #
-# Authour MikeC@kavinjka.com
+# Author MikeC@kavinjka.com
 
 import RPi.GPIO as GPIO
 import time # for sleep functions
@@ -19,20 +19,18 @@ locale_usa = True
 # assume we want the beep On (as it's really loud when you are developing we can turn it off with the --no_beep argument)
 beep = True
 
-# check for any Command Line Arguments (this is really bad "Hard Coding")
-if (len(argv) >= 2):
-    print(argv[1])
-    if (argv[1] == "--scotland"):
-       locale_usa = False
-    if (argv[1] == "--no_beep"):
-        beep = False
+# use a database_file variable in case we ever want to change it
+database_file = 'pi-traffic-lights.db'
 
-if (len(argv) == 3):
-    print(argv[2])
-    if (argv[2] == "--scotland"):
+# check for any Command Line Arguments (this is a little better)
+for i in range(1, len(argv)): # ignore argv[0] as it is the file name
+    print(argv[i])
+    if (argv[i] == "--scotland"):
         locale_usa = False
-    if (argv[2] == "--no_beep"):
+    if (argv[i] == "--no_beep"):
         beep = False
+    if (argv[i] == '--database'): # technically has to be last  pair of argv elements
+        database_file = argv[i+1]
 
 # define the GPIO port for the Button
 BUTTON = 16
@@ -55,29 +53,29 @@ previous_input = 0
 crossing = False
 
 # Define an exit handler for the program (called on Cntrl-C)
-def CntrlCHandler(signal_received, frame):
+def ctrl_c_handler(signal_received, frame):
     # Handle any GPIO cleanup here
     print('SIGINT or CTRL-C detected. Exiting gracefully')
     GPIO.cleanup()
     connection.close() # to close out Sqlite3
     exit(0)
 
-def InitializeGPIO():
+def initialize_GPIO():
     # Initialize the GPIO Infrastructure to BCM (Broadcom SOC channel)
     GPIO.setmode(GPIO.BCM)
 
     # Setup all the Input and Output GPIO Pins
     GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 16 (button) to be an input pin and set initial value to be pulled low (off)
     GPIO.setup(RED, GPIO.OUT)
-    GPIO.setup(GREEN, GPIO.OUT) 
+    GPIO.setup(GREEN, GPIO.OUT)
     GPIO.setup(AMBER, GPIO.OUT)
     GPIO.setup(COMBI_RED, GPIO.OUT)
     GPIO.setup(COMBI_GREEN, GPIO.OUT)
-    GPIO.setup(COMBI_BLUE, GPIO.OUT) 
+    GPIO.setup(COMBI_BLUE, GPIO.OUT)
     GPIO.setup(BUZZER, GPIO.OUT)
 
 # turn on the Green Light and red cross walk light, Assume the Cross-Walk is Cars allowed through.
-def InitializeCrossWalk():
+def initialize_cross_walk():
     GPIO.output(GREEN, GPIO.HIGH)
     GPIO.output(AMBER, GPIO.LOW)
     GPIO.output(RED, GPIO.LOW)
@@ -87,7 +85,7 @@ def InitializeCrossWalk():
     GPIO.output(BUZZER, GPIO.LOW)
 
 # Get the Unique MAC address of this Raspberry Pi for a given Network Interface
-def getMAC(interface='eth0'):
+def get_MAC(interface='eth0'):
   # Return the MAC address of the specified interface
   try:
     str = open('/sys/class/net/%s/address' %interface).read()
@@ -96,28 +94,42 @@ def getMAC(interface='eth0'):
   return str[0:17]
 
 # Setup the Sqlite3 connection and curosr to pi-traffic-lights.db
-connection = sqlite3.connect('pi-traffic-lights.db')
+connection = sqlite3.connect(database_file)
 cursor = connection.cursor()
 
-# Use a function to control the Car to Pedestrian Transition 
-def CarToPedestrian(transition_time):
+# Use a function to control the Car to Pedestrian Transition
+def car_to_pedestrian(transition_time):
+    # Add a short sleep so there is time from the button release to the start of the transition
+    time.sleep(1)
+
+    # Transition from Green to Amber
     GPIO.output(GREEN, GPIO.LOW)
     GPIO.output(AMBER, GPIO.HIGH)
 
-    time.sleep(transition_time) # sleep between Green/Amber/Red
+    # sleep between Amber/Red
+    time.sleep(transition_time)
 
+    # Transition from Amber to Red
     GPIO.output(AMBER, GPIO.LOW)
     GPIO.output(RED, GPIO.HIGH)
 
+    # Add a short sleep to let the Red to Car drivers be in place
+    time.sleep(1)
+
 # Use a function to control the Pedestrian to Car transition
-def PedestrianToCar(transition_time, locale_usa):
+def pedestrian_to_car(transition_time, locale_usa):
     GPIO.output(COMBI_RED, GPIO.HIGH) # Red Do Not Walk signal to Pedestrians
-    
+    # Add a short sleep  after Pedistian signal goes Red before moving on
+    time.sleep(1)
+
     # As the USA and Scotland return to car sequence is different we need to decide
     if locale_usa:
-        time.sleep(transition_time) 
+        time.sleep(transition_time)
         GPIO.output(RED, GPIO.LOW) # Red goes straight to Green in the USA
     else:
+        # Add a small sleep before tranitioning to Red/Amber
+        time.sleep(1)
+
         GPIO.output(AMBER, GPIO.HIGH) # and hold for transition_time
 
         # Scotland Sequence is a little different with Red/Amber on Prior to flip to Green
@@ -131,7 +143,7 @@ def PedestrianToCar(transition_time, locale_usa):
     # return the crossing to Cars ("Proceed if safe to do so")
     GPIO.output(GREEN, GPIO.HIGH)
 
-def Beep(duration):
+def beep_function(duration):
     if (beep == True):
         GPIO.output(BUZZER, GPIO.HIGH)
 
@@ -140,7 +152,7 @@ def Beep(duration):
     GPIO.output(BUZZER, GPIO.LOW)
 
 # use a function to Start and maintain the Walk Signal (White in the USA, Green in Scotland with an intermitent "beep')
-def WalkSignal(transition_time, locale_usa):
+def walk_signal(transition_time, locale_usa):
     if (locale_usa): # USA Walk signal is solid white (R+B+G)
         GPIO.output(COMBI_RED, GPIO.HIGH)
         GPIO.output(COMBI_BLUE, GPIO.HIGH)
@@ -149,10 +161,10 @@ def WalkSignal(transition_time, locale_usa):
         GPIO.output(COMBI_GREEN, GPIO.HIGH)
 
     for i in range(1, transition_time):
-        Beep(0.1)
+        beep_function(0.1)
         time.sleep(0.5)
 
-def EndWalkSignal(transition_time, locale_usa):
+def end_walk_signal(transition_time, locale_usa):
     # Shut of the walk signal
     GPIO.output(COMBI_RED, GPIO.LOW)
     GPIO.output(COMBI_BLUE, GPIO.LOW)
@@ -161,46 +173,46 @@ def EndWalkSignal(transition_time, locale_usa):
     if  (locale_usa):
         for i in range(1, transition_time):
            GPIO.output(COMBI_RED, GPIO.HIGH)
-           Beep(0.2)
+           beep_function(0.2)
            GPIO.output(COMBI_RED, GPIO.LOW)
            time.sleep(0.5)
     else: # Scotland leaves the Green Light on throughout but good to change the Beep Frequency
         GPIO.output(COMBI_GREEN, GPIO.HIGH)
         for i in range(1, transition_time):
-            Beep(0.2)
+            beep_function(0.2)
             time.sleep(0.5)
-            
-        # When done Turn off Green and Trun on Red 
+
+        # When done Turn off Green and Trun on Red
         GPIO.output(COMBI_GREEN, GPIO.LOW)
         GPIO.output(COMBI_RED, GPIO.HIGH)
 
 # Main Program
-signal(SIGINT, CntrlCHandler)
-InitializeGPIO()
-InitializeCrossWalk()
-pi_id = getMAC('wlan0')
+signal(SIGINT, ctrl_c_handler)
+initialize_GPIO()
+initialize_cross_walk()
+pi_id = get_MAC('wlan0')
 
 # infinite loop for the main thread (Use Cntrl-C to exit)
 while True:
   # take a reading from the switch
   input = GPIO.input(BUTTON)
- 
+
   # if the last reading was high and this one is low, signal Button Released
   if ((previous_input) and not input):
-    currentDateTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print("Button Released at", currentDateTime)
+    current_date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("Button Released at", current_date_time)
 
-    # and update the database
-    cursor.execute('INSERT INTO button_pressed VALUES(?,?)', (pi_id, currentDateTime,))
+    # Update the database
+    cursor.execute('INSERT INTO button_pressed VALUES(?,?,?,?)', (pi_id, current_date_time, locale_usa, beep))
     connection.commit()
 
-    CarToPedestrian(5)
+    car_to_pedestrian(5)
 
-    WalkSignal(10, locale_usa)
+    walk_signal(10, locale_usa)
 
-    EndWalkSignal(10, locale_usa)
+    end_walk_signal(10, locale_usa)
 
-    PedestrianToCar(5, locale_usa)
+    pedestrian_to_car(5, locale_usa)
 
   # update prev_input value
   previous_input = input
