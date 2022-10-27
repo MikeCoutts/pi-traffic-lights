a6d6b04cc74edc370bfe113d61e249d0a40c4011
MikeCoutts <Mikec@Kavinjka.com>
2022-10-27 05:17:12 +0100

    fix up the pi-visualize.pi after bad merge

--- pi-visualise-data.py
+++ pi-visualise-data.py
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
-import sqlite3 
-=======
 import sqlite3
->>>>>>> 80db94211802b6b7a38ce0b4fba85b440c1fde54
+
 import pandas as pd
 import streamlit as st
 import altair as alt
 
-<<<<<<< HEAD
 # create a connection to the pi-traffic-lights.db sqlite3 Database
-=======
-# create a connection to the pi-traffic-lights sqlite3 Database
->>>>>>> 80db94211802b6b7a38ce0b4fba85b440c1fde54
 conn = sqlite3.connect('pi-traffic-lights.db')
 
 # create a sidebar date selector
 date = st.sidebar.date_input("Button Pressed Date")
 
-<<<<<<< HEAD
 # get the earliest and latest dates in the data set
 earliestDate = pd.read_sql(
     "SELECT MIN(pressed_on) as EarliestDate from button_pressed",
@@ -39,7 +31,7 @@ c = alt.Chart(df).transform_fold( # fold USA/Beep into a single "value" column
 ).mark_bar().encode(
      x=alt.X('pressed_on:T', timeUnit='date'), # X Axis is a day based time parameter 
      y='value:Q', # Y Axis is the combined USA/Beep Value stack as a Quantatative param
-=======
+
 # Get the earliest and latest date available in the data set
 earliestDate = pd.read_sql("SELECT MIN(pressed_on)
                 as EarliestDate from button_pressed", con=conn)
@@ -64,13 +56,11 @@ c = alt.Chart(df).transform_fold( # fold USA/Beep into "value" column
      x=alt.X('pressed_on:T', timeUnit='date'),
      # Y Axis uses the combined usa/beep "value" as a Quantatative parameter
      y='value:Q',
->>>>>>> 80db94211802b6b7a38ce0b4fba85b440c1fde54
      color='column:N')
 
 # Display the chart data using the altair_chart function from within streamlit
 st.altair_chart(c)
 
-<<<<<<< HEAD
 # Create a 2nd Pandas DataFrame (dateBasedDF) for the selected date by reading the button_pressed table
 dateBasedQuery ="SELECT * from button_pressed where pressed_on LIKE '" + date.strftime("%Y-%m-%d%") + "'"
 
@@ -82,7 +72,7 @@ st.write("DataFrame for ", date.strftime("%Y-%m-%d"))
 
 # And output the dateBased Data Frame
 st.write(dateBasedDF)
-=======
+
 # Create a SQLite3 query for all the data for a specific day
 dateBasedQuery = "SELECT * from button_pressed
                   where pressed_on LIKE
@@ -93,7 +83,6 @@ dateBasedData = pd.read_sql(dateBasedQuery, con=conn)
 
 st.write("DataFrame for ", date.strftime("%Y-%m-%d"))
 st.write(dateBasedData)
->>>>>>> 80db94211802b6b7a38ce0b4fba85b440c1fde54
 
 # close off the connection to sqlite3
 conn.close()
